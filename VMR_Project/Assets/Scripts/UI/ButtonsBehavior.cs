7945f23f4523ed60141c07f2ae4ddcba6595220a
angelsofy23 <sofiabilyk23@gmail.com>
2025-02-17 20:32:54 +0000

    Win and Lose panels

--- VMR_Project/Assets/Scripts/UI/ButtonsBehavior.cs
+++ VMR_Project/Assets/Scripts/UI/ButtonsBehavior.cs
@@ -87,4 +87,9 @@ public class ButtonsBehavior : MonoBehaviour
     {
         howToPlayPanel.SetActive(false);
     }
+    
+    public void Repeat()
+    {
+        SceneManager.LoadScene("GameSingleplayer");
+    }
 }
